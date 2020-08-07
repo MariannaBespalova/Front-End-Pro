5b6d56bbd87ee4749397e1fe625514713954a7d3
marianna <mariannabespalova9@gmail.com>
2020-08-08 01:38:25 +0300

    changed let to const

--- js/home_task_5/bespalova_ht5_2.js
+++ js/home_task_5/bespalova_ht5_2.js
@@ -1,7 +1,7 @@
 //Написать собственную реализацию функций map.
 
 function map(array, callback) {
-  let result = [];
+  const result = [];
   for (const element of array) {
     const newElement = callback(element);
     result.push(newElement);
@@ -9,7 +9,7 @@ function map(array, callback) {
   return result;
 }
 
-let array = [8, 7, 9, 3, 2];
+const array = [8, 7, 9, 3, 2];
 
 function prodOnTwo(element) {
   return element * 2;
--- js/home_task_5/bespalova_ht5_3.js
+++ js/home_task_5/bespalova_ht5_3.js
@@ -1,20 +1,20 @@
 //Написать собственную реализацию функций filter
 
 function filter(array, callback) {
-  let result = [];
+  const result = [];
   for (const element of array) {
     if (callback(element)) {
       result.push(element);
-    };
+    }
   }
   return result;
 }
 
-let array = [8, 7, 9, 3, 2];
+const array = [8, 7, 9, 3, 2];
 
 function isEven(element) {
   return element % 2 === 0;
 }
 
-let newArray = filter(array, isEven)
+const newArray = filter(array, isEven)
 console.log(newArray);
