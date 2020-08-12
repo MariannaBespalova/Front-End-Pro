496d7d3ed20fda09ce19ed00d530f38d18c6a2dd
marianna <mariannabespalova9@gmail.com>
2020-08-12 14:02:16 +0300

    ternary operator added

---
+++ js/home_task_7/bespalova_ht7_3.js
@@ -0,0 +1,18 @@
+// Написать функцию, которая принимает строку и возвращает данные о том,
+// сколько раз встречается каждая буква. Например, если передали строку “aaabbc”,
+// то функция должна сообщить, что буква “a” встретилась 3 раза,
+// буква “b” встретилась 2 раза, а буква “c” - 1 раз.
+// Функция не должна использовать console.log
+
+function calcEachLetter(str) {
+  const newObject = {};
+  for (let i = 0; i < str.length; i++) {
+    const key = str[i];
+    newObject[key] =
+      newObject[key] === undefined ? (newObject[key] = 1) : newObject[key] + 1;
+  }
+  return newObject;
+}
+
+const str = "aaabbc";
+console.log(calcEachLetter(str));
