5b0daf59d7ad3a86353729525091bf5fa4a05199
marianna <mariannabespalova9@gmail.com>
2020-08-25 00:23:36 +0300

    added new solutions

--- js/home_task_7/bespalova_ht7_3.js
+++ js/home_task_7/bespalova_ht7_3.js
@@ -6,13 +6,22 @@
 
 function calcEachLetter(str) {
   const newObject = {};
-  for (let i = 0; i < str.length; i++) {
-    const key = str[i];
-    newObject[key] =
-      newObject[key] === undefined ? (newObject[key] = 1) : newObject[key] + 1;
+  for (let letter of str) {
+    count = newObject[letter];
+    newObject[letter] = count === undefined ? 1 : count + 1;
+    console.log(newObject);
   }
   return newObject;
 }
 
 const str = "aaabbc";
 console.log(calcEachLetter(str));
+
+function countLettersOfString2(str) {
+  const result = {};
+  for (const letter of str) {
+    const count = result[letter] || 0;
+    result[letter] = count + 1;
+  }
+  return result;
+}
