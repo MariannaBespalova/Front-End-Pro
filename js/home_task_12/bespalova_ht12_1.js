92179fb2c8537df384cac72bef8783d9fd22abc1
marianna <mariannabespalova9@gmail.com>
2020-09-06 23:58:34 +0300

    ht12

--- js/home_task_12/bespalova_ht12_1.js
+++ js/home_task_12/bespalova_ht12_1.js
@@ -45,18 +45,56 @@ function reduceOnButton() {
 
 reduceOnButton();
 
-// Дано 5 квадратов. Каждый по клику меняет цвет (синий -> зеленый -> желтый)
+// Дано 5 квадратов. Каждый по клику меняет цвет (синий -> зеленый -> желтый
 
-function changeColor() {
-  const colors = ["blue", "green", "yellow"];
+const colorsArray = ["blue", "green", "yellow"];
 
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("click", () => {
-      for (const color of colors) {
-        squares[i].style.backgroundColor = color;
-      }
-    });
-  }
+const lastIndexInArray = colorsArray.length - 1;
+
+for (const square of squares) {
+  square.style.backgroundColor = colorsArray[0];
+  square.addEventListener("click", () => {
+    let currentColor = square.style.backgroundColor;
+    let currentColorIndex = colorsArray.indexOf(currentColor);
+
+    if (currentColorIndex !== lastIndexInArray) {
+      currentColorIndex++;
+      square.style.backgroundColor = colorsArray[currentColorIndex];
+    } else if (currentColorIndex >= lastIndexInArray) {
+      square.style.backgroundColor = colorsArray[0];
+    }
+  });
 }
+// const colors = ["blue", "green", "yellow"];
 
-changeColor();
+// for (const square of squares) {
+//   square.style.backgroundColor = "blue";
+//   square.addEventListener("click", () => {
+//     if (square.style.backgroundColor === "blue") {
+//       square.style.backgroundColor = "green";
+//     } else if (square.style.backgroundColor === "green") {
+//       square.style.backgroundColor = "yellow";
+//     } else if (square.style.backgroundColor === "yellow") {
+//       square.style.backgroundColor = "blue";
+//     }
+//   });
+// }
+
+// Сделать список. По клику на любой из элементов он выделяется красным цветом.
+// Если один уже выделен, а кликнули по другому, то выделение с прошлого снимается,
+// и ставится на тот, по которому кликнули.
+
+const list = document.querySelector("ul");
+const listItems = document.querySelectorAll("li");
+
+let selectedLi;
+
+for (const listItem of listItems) {
+  listItem.addEventListener("click", (event) => {
+    if (selectedLi) {
+      selectedLi.classList.remove("red");
+    }
+    selectedLi = event.target;
+    event.target.classList.add("red");
+  });
+}
