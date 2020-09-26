b595ade93f76d87d449fa5356e09fc35fc572cf3
marianna <mariannabespalova9@gmail.com>
2020-09-26 20:46:50 +0300

    ht15

--- js/home_task_15/bespalova_ht15_1.js
+++ js/home_task_15/bespalova_ht15_1.js
@@ -1,53 +1,150 @@
+const wrapper = document.querySelector(".wrapper");
+if (wrapper !== null) {
+	const dropdown = createDiv({ className: "dropdown" });
+	const dropButton = createButton({ text: "Choose clock format", className: "dropbtn" });
+	const dropdownContent = createDiv({ className: "dropdown-content" });
+	const clock = createDiv({ className: "clock" });
 
-const clock = document.querySelector(".wrapper");
+	const arrOfFormats = [localTime, timeAmPm, timeReverse];
+	const arrOfHref = ["localTime", "timeAmPm", "timeReverse"];
+	const nameforLinks = ["Local Time", "Time AM/PM", "Time Reverse"];
 
-for (let i = 0; i < 8; i++) {
-  const divs = document.createElement("div");
-  clock.appendChild(divs);
+	for (let i = 0; i < arrOfFormats.length; i++) {
+		let links = createLink({ href: `#${arrOfHref[i]}`, text: nameforLinks[i], className: "format-clock" })
+		dropdownContent.appendChild(links);
+	}
+	const links = dropdownContent.children
+
+	dropButton.addEventListener("click", () => {
+		dropdownContent.classList.toggle("show");
+	})
+	window.onclick = function (event) {
+		if (!event.target.matches('.dropbtn')) {
+			if (dropdownContent.classList.contains('show')) {
+				dropdownContent.classList.remove('show');
+			}
+		}
+	}
+
+	let selectedLink;
+	for (let i = 0; i < arrOfFormats.length; i++) {
+		let functionName = arrOfFormats[i];
+		console.log(links[i])
+		links[i].addEventListener('click', () => {
+			if (selectedLink !== undefined) {
+				clearInterval(selectedLink)
+			}
+			selectedLink = setInterval(() => {
+				showFormat(functionName());
+			}, 1000)
+		})
+	}
+
+	function showFormat(clockFormat) {
+		console.clear();
+		console.log(clockFormat)
+		clock.innerHTML = "";
+
+		for (let i = 0; i < clockFormat.length; i++) {
+			const divs = document.createElement("div");
+			clock.appendChild(divs);
+		}
+
+		const divsForDigit = document.querySelectorAll(".clock div");
+		for (let i = 0; i < clockFormat.length; i++) {
+			divsForDigit[i].innerHTML = clockFormat[i];
+		}
+
+		const colors = ["blue", "yellow", "green", "red", "purple", "pink", "orange", "grey"];
+		for (let i = 0; i < divsForDigit.length; i++) {
+			divsForDigit[i].style.color = colors[i];
+		}
+	}
+
+	wrapper.appendChild(dropdown);
+	wrapper.appendChild(clock);
+	dropdown.appendChild(dropButton);
+	dropdown.appendChild(dropdownContent);
 }
 
-const colors = ["blue", "yellow", "green", "red", "purple", "pink", "orange", "grey"];
-const divsForDigit = document.querySelectorAll(".wrapper div");
+function localTime() {
+	let currentTime = new Date();
+	let sec = currentTime.getSeconds();
+	let min = currentTime.getMinutes();
+	let hours = currentTime.getHours();
 
-for (let i = 0; i < divsForDigit.length; i++) {
-  divsForDigit[i].style.color = colors[i];
+	if (hours < 10) hours = `0${hours}`;
+	if (min < 10) min = `0${min}`;
+	if (sec < 10) sec = `0${sec}`;
+
+	return `${hours}:${min}:${sec}`;
 }
 
-function myClock() {
-  let date = new Date();
-  let sec = date.getSeconds();
-  let min = date.getMinutes();
-  let hours = date.getHours();
+function timeAmPm() {
+	let currentTime = new Date();
+	let sec = currentTime.getSeconds();
+	let min = currentTime.getMinutes();
+	let hours = currentTime.getHours();
 
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  if (min < 10) {
-    min = `0${min}`;
-  }
-  if (sec < 10) {
-    sec = `0${sec}`;
-  }
+	var amPm = hours >= 12 ? 'pm' : 'am';
+	if (min < 10) min = `0${min}`;
+	if (sec < 10) sec = `0${sec}`;
+	hours = hours % 12;
+	hours = hours ? hours : 12;
+	if (hours < 10) hours = `0${hours}`;
 
-  const formatClock = `${hours}:${min}:${sec}`;
-  
-  let arrOfDigits = formatClock.split('');
-  console.log(arrOfDigits);
+	return `${hours}:${min}:${sec}${amPm}`;
+}
 
-  for (let i = 0; i < arrOfDigits.length; i++) {
-    divsForDigit[i].innerHTML = arrOfDigits[i];
-  }
+function timeReverse() {
+	let currentTime = new Date();
+	let sec = currentTime.getSeconds();
+	let min = currentTime.getMinutes();
+	let hours = currentTime.getHours();
 
-  console.clear();
-  console.log(formatClock);
+	if (hours < 10) hours = `0${hours}`;
+	if (min < 10) min = `0${min}`;
+	if (sec < 10) sec = `0${sec}`;
+
+	return `${sec}:${min}:${hours}`;
 }
 
 
-function time(functime) {
-  let start;
-  start = setInterval(functime, 1000);
-  functime();
+function createButton(props) {
+	const text = props.text || "";
+	const className = props.className || "";
+
+	const button = document.createElement("button");
+
+	button.type = "button";
+	button.innerHTML = text;
+	button.className = className;
+
+	return button;
 }
 
-time(myClock);
+function createDiv(props) {
+	const text = props.text || "";
+	const className = props.className || "";
 
+	const div = document.createElement("div");
+
+	div.innerHTML = text;
+	div.className = className;
+
+	return div;
+}
+
+function createLink(props) {
+	const text = props.text || "";
+	const className = props.className || "";
+	const href = props.href || "";
+
+	const link = document.createElement("a");
+
+	link.innerHTML = text;
+	link.className = className;
+	link.href = href;
+
+	return link;
+}
