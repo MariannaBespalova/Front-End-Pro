7de89134e3fd259ab02a330b04903e652f5b5e1f
marianna <mariannabespalova9@gmail.com>
2020-10-12 12:02:47 +0300

    ht19

---
+++ js/home_task_19/bespalova_ht19_1.js
@@ -0,0 +1,182 @@
+const BASE_URL = "https://rickandmortyapi.com/api/character/"
+const wrapper = document.querySelector(".wrapper")
+
+const buttonTypes = Object.freeze({
+  prev: "prev",
+  next: "next"
+});
+
+class Button {
+  constructor(props) {
+    this._button = document.createElement("button");
+    this._button.innerText = props.innerText || "";
+    this._button.className = props.className || "";
+    this._button.dataset.type = props.dataset || "";
+  }
+
+  setDisabled(disabled) {
+    this._button.disabled = disabled;
+  }
+
+  onClick(callback) {
+    this._button.addEventListener("click", callback)
+  }
+
+  render() {
+    return this._button;
+  }
+}
+
+class Text {
+  constructor(props) {
+    this._text = document.createElement("span");
+    this._text.className = props.className || "";
+  }
+
+  set text(value) {
+    this._text.innerText = value;
+  }
+
+  render() {
+    return this._text
+  }
+}
+
+class ListItem {
+  constructor(props) {
+    this._listItem = document.createElement("li");
+    this._span = document.createElement("span")
+    this._span.innerText = props.spanText || "";
+    this._image = document.createElement("img");
+    this._image.src = props.src || "";
+    this._listItem.className = props.className || "";
+  }
+
+  render() {
+
+    this._listItem.appendChild(this._span)
+    this._listItem.appendChild(this._image)
+    return this._listItem;
+  }
+}
+
+class List {
+  constructor() {
+    this._list = document.createElement("ul");
+
+    this._prevButton = new Button({ innerText: "PREV", className: "btn" });
+    this._nextButton = new Button({ innerText: "NEXT", className: "btn" });
+    this._label = new Text({ className: "label" });
+  }
+
+  addList(text, value) {
+    const li = new ListItem({
+      className: "list_item",
+      src: value,
+      spanText: text,
+    })
+
+    this._list.appendChild(li.render());
+  }
+
+  setLabel(text) {
+    this._label.text = text;
+  }
+
+  clear() {
+    this._list.innerHTML = "";
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.className = "list";
+
+    wrapper.appendChild(this._list);
+    wrapper.appendChild(this._prevButton.render());
+    wrapper.appendChild(this._label.render());
+    wrapper.appendChild(this._nextButton.render())
+
+    return wrapper;
+  }
+}
+
+class AjaxList extends List {
+  constructor(baseUrl) {
+    super();
+    this.baseUrl = baseUrl;
+    this.page = 1;
+    this.data = {};
+  }
+
+  load() {
+    const searchParams = new URLSearchParams();
+    searchParams.set("page", this.page);
+
+    fetch(`${this.baseUrl}?${searchParams}`)
+      .then(response => {
+          return response.json();
+      })
+      .then(result => {
+        this.data = {
+          hasNextPage: result.info.next !== null,
+          hasPrevPage: result.info.prev !== null,
+          result: result.results
+        }
+        this.onDataLoad()
+      })
+      .catch(error => console.error("Something went wrong"));
+  }
+
+  onDataLoad() {
+    this.clear();
+    this.setLabel(this.page);
+
+    this._prevButton.setDisabled(!this.data.hasPrevPage);
+    this._nextButton.setDisabled(!this.data.hasNextPage);
+
+  }
+
+  onNextButtonClick() {
+    this.page++;
+    this.load();
+  }
+
+  onPrevButtonClick() {
+    this.page--;
+    this.load();
+  }
+
+  render() {
+    this._prevButton.onClick(this.onPrevButtonClick.bind(this));
+    this._nextButton.onClick(this.onNextButtonClick.bind(this));
+    this.load();
+    return super.render()
+  }
+}
+
+class CharacterList extends AjaxList {
+  constructor() {
+    super(BASE_URL);
+  }
+
+  onDataLoad() {
+    super.onDataLoad()
+
+    const listElements = this.data.result.map(item => {
+      return {
+        text: `${item.name}; status: ${item.status}`,
+        image: item.image
+      };
+    })
+
+    // const listElements = this.data.result.map(item => ({
+    //   text: `Status: ${item.status}, name: ${item.name}`,
+    //   image: item.image
+    // }));
+
+    listElements.forEach(element => this.addList(element.text, element.image));
+  }
+}
+
+const list = new CharacterList();
+document.body.appendChild(list.render())
